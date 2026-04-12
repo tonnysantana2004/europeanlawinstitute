aad9bc1c214bf4ee53e1805eddbefed700967b31
Tonny Santana <tonnysantanasantos@gmail.com>
2026-04-12 15:32:54 -0300

    feat: add SVG inline content storage in post meta on attachment preparation

--- functions.php
+++ functions.php
@@ -32,6 +32,34 @@ add_filter(
 	}
 );
 
+// Sempre que tiver uma consulta de post do tipo attatchment, checar se é um SVG.
+// Se for, pegar o conteúdo, sanatizar e salvar em um post meta personalizado.
+add_filter(
+	'wp_prepare_attachment_for_js',
+	function ( $response ) {
+
+		// Is an svg.
+		if ( 'image/svg+xml' !== $response['mime'] ) {
+			return $response;
+		}
+
+		// The inline svg has not been stored yet.
+		if ( get_post_meta( $response['id'], '_eli_inline_svg', true ) ) {
+			return $response;
+		}
+
+		// Set the inline svg custom field.
+		$svg_url     = get_attached_file( $response['id'] );
+		$svg_content = file_get_contents( $svg_url ); // phpcs:ignore
+
+		// Todo: implement an SVG sanitizer.
+		update_post_meta( $response['id'], '_eli_inline_svg', $svg_content );
+
+		return $response;
+	},
+	10
+);
+
 // TODO: Remove once the development is finished.
 remove_action( 'wp_footer', 'the_block_template_skip_link' );
 
